1cb6e09c027c415b07c54e95c348e075a3bf7f9f
Deborah Lewis <deborahlewis@users.noreply.github.com>
2016-09-09 10:43:02 -0400

    Add files via upload

---
+++ Deitel_Fig2_1.java
@@ -0,0 +1,16 @@
+/*
+ * Deborah Lewis
+ * Figure 2.1 Deitel: How to Program (Early Ojects)
+ * Text-Printing program
+*/
+package deitel_fig2_1;
+
+
+public class Deitel_Fig2_1 {
+
+// main method begins execution of Java application
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming!");
+    } //end main method
+
+} //end class
